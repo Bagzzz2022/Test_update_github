36e26ad090b495df5a47d816d5e95440a28a4921
Bagzzz2022 <93910447+Bagzzz2022@users.noreply.github.com>
2025-08-05 10:20:41 +0300

    add files for test

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from routes import json_export, md_full, md_clean, md_clean_cut, md_clean_single, md_clean_depth, md_clean_multiple, zip_export, csv_export, excel_export, opml_export, mm_export, xmind_export, pyrus_upload, xmind_procedures, generate_kpi, xmind_sync_github
+from routes.call_pyrus_upload import router as call_pyrus_upload_router
+
+app = FastAPI()
+
+app.include_router(json_export.router)
+app.include_router(md_full.router)
+app.include_router(md_clean.router)
+app.include_router(md_clean_cut.router)
+app.include_router(md_clean_single.router)
+app.include_router(md_clean_depth.router)
+app.include_router(md_clean_multiple.router)
+app.include_router(zip_export.router)
+app.include_router(csv_export.router)
+app.include_router(excel_export.router)
+app.include_router(opml_export.router)
+app.include_router(mm_export.router)
+app.include_router(xmind_export.router)
+app.include_router(pyrus_upload.router)
+app.include_router(xmind_procedures.router)
+app.include_router(call_pyrus_upload_router)
+app.include_router(generate_kpi.router)
+app.include_router(xmind_sync_github.router)
---
+++ routes/xmind_sync_github.py
@@ -0,0 +1,35 @@
+from github import Github
+from github.InputGitAuthor import InputGitAuthor
+from fastapi import APIRouter, Body
+import requests
+import os
+
+router = APIRouter()
+
+@router.post("/xmind-sync")
+async def xmind_sync(url: str = Body(...)):
+    print(url)
+
+    g = Github('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')
+
+    owner = 'Tiffozi-ilia'
+    repo_name = 'AImatrix'
+    file_path = 'matrix.xmind'
+    branch = 'all-in'
+
+    repo = g.get_repo(f"{owner}/{repo_name}")
+    contents = repo.get_contents(file_path, ref=branch)
+
+    response = requests.get(url)
+    new_content = response.content
+
+    # with codecs.open(file, 'rb') as f:
+    #     new_content = f.read()
+
+    repo.update_file(
+        contents.path,
+        "Upload via DiFyBot",
+        new_content,
+        contents.sha,
+        branch=branch,
+        author=InputGitAuthor("DiFyBot", "example@somemail.com"))
